4fcc86471d8f43e93ac2061ab2cbbeb54a047bf5
Ntye <ntye.nina@gmail.com>
2024-06-05 09:05:58 +0100

    start landing page

---
+++ components/main/ActionWidget.jsx
@@ -0,0 +1,15 @@
+'use client'
+import {FaBars} from "react-icons/fa";
+import Link from "next/link";
+import React from "react";
+
+const SideNavbar = ({ name, color, link }) => {
+
+  return (
+    <div>
+      Something About the project I think
+    </div>
+  );
+};
+
+export default SideNavbar;
